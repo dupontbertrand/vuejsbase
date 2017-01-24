40a987e5bce05c41d1abc9d820b68dae656bf167
dupontbertrand <dupontbertrand59@gmail.com>
2017-01-24 20:20:19 +0100

    changes on readme, add personal tests not on initial video

--- 01-Decouverte/app.js
+++ 01-Decouverte/app.js
@@ -4,7 +4,27 @@ new Vue ({
   data: {
     message: 'Salut les gens',
     link1: 'http://www.google.fr',
-    link2: 'http://www.twitter.fr'
+    link2: 'http://www.twitter.fr',
+    success1: true,
+    success2: true,
+    persons: ['Bertrand', 'Ivan', 'Wilfried', 'Yoann', 'Ophélie', 'GuillaumeG', 'GuillaumeL', 'Ahrry', 'Robin', 'Romain']
+  },
+
+  methods: {
+    changestate: function () {
+      if (this.success1 === true) {
+      this.success1 = false
+      }
+      else {
+      this.success1 = true
+      }
+    },
+    close: function () {
+      this.success2 = false
+    },
+    open: function () {
+      this.success2 = true
+    },
   }
 
 })
